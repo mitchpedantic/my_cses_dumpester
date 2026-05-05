1b355c89552524bae3dd147d09f7785b9bbd97ac
Michele <michele@micheleUbuntu.lan>
2026-05-05 18:14:38 +0200

    Less control, less variables, less operations

--- intro_set/increasing_set/increasing.c
+++ intro_set/increasing_set/increasing.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-__uint64_t max, iter, scn, inc, count = 0, pre = 0U;
+__uint64_t max, scn, inc, count = 0, pre = 0U;
 
 int main(){
     scanf("%lu", &max);
-    for (iter = (max/200001U)*(max); iter < max; iter++) {
+    while ((max--) > 0) {
         scanf("%lu", &scn);
         inc = ((__uint64_t)(pre>scn))*(pre-scn);
         count += inc; pre=scn+inc;
